4218d532d547944f5e114e2c8d617e8b834d6ee9
erasmoadorno <erastuci@gmail.com>
2022-12-19 20:51:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Calculator from "./components/aulas/calculadora simples/main";
+
+
+
+
+function App() {
+  return <>
+  <Calculator></Calculator>
+  </>;
+}
+export default App;
